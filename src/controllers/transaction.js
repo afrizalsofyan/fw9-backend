003271c69a1111981d3dc9d57d4e3f4f2e4253a8
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-28 18:22:16 +0700

    fix: update user and transaction controller

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,9 +1,9 @@
-const dataDummy = require('../../dummy_data')
-const response = require('../helpers/standartResponse')
+const dataDummy = require('../../dummy_data');
+const response = require('../helpers/standartResponse');
 
 exports.getTransaction = (req, res) => {
-    const data = dataDummy["transaction"]
-    let date = data.map(el=> el["date_transaction"])
-    console.log(date);
-    return response(res, "this is transaction data.", 200, date)
-}
+  const data = dataDummy['transaction'];
+  let date = data.map(el=> el['date_transaction']);
+  console.log(date);
+  return response(res, 'this is transaction data.', 200, date);
+};
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,7 @@
-const dataDummy = require('../../dummy_data')
-const response = require('../helpers/standartResponse')
-
+const response = require('../helpers/standartResponse');
+const userModel = require('../models/users');
 exports.getUser = (req, res) => {
-    const data = dataDummy["username"]
-    console.log(data);
-    return response(res, "This is user data.", 200, data)
-}
+  userModel.getAllUsers((result)=>{
+    return response(res, 'This is user dataaaa.', result,200);
+  });
+};
