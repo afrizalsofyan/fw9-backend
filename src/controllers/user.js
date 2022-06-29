d90f4424d9a1579436ec3d459edcd547797ee8c5
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-29 16:15:36 +0700

    fix: update crud logic controller feature

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,45 @@
 const response = require('../helpers/standartResponse');
 const userModel = require('../models/users');
-exports.getUser = (req, res) => {
+
+// const db = require('../helpers/db');
+
+exports.getAllUser = (req, res) => {
   userModel.getAllUsers((result)=>{
     return response(res, 'This is user dataaaa.', result,200);
+  });
+};
+
+exports.getUser = (req, res) => {
+  const {id} = req.params;
+  userModel.getUser(id, (result)=>{
+    return response(res, 'This your selected data.', result);
+  });
+  
+};
+
+exports.createUser = (req, res) => {
+  userModel.createUser(req.body, (result)=>{
+    return response(res, 'Create user successully!!', result[0]);
+  });
+};
+
+exports.updateUser = (req, res) => {
+  const {id} = req.params;
+  userModel.updateUsers(id, req.body, (result)=>{
+    return response(res, 'Update user data is success!!', result[0]);
+  });
+};
+
+exports.hardDeleteUser = (req, res) => {
+  const {id} = req.params;
+  userModel.hardDeleteUser(id, (result)=>{
+    return response(res, 'Delete user is success!!', result);
+  });
+};
+
+exports.softDeleteUser = (req, res) => {
+  const {id} = req.params;
+  userModel.softDeleteUser(id, (result)=>{
+    return response(res, 'Delete data is success!!', result[0]);
   });
 };
