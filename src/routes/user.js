c3d963bc8d0a958bce40aad7672b065cc328f8af
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-29 16:16:36 +0700

    fix: update method CRUD routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,11 @@ const users = require('express').Router();
 
 const userController = require('../controllers/user');
 
-users.get('/', userController.getUser);
+users.get('/', userController.getAllUser);
+users.get('/:id', userController.getUser);
+users.post('/', userController.createUser);
+users.patch('/:id', userController.updateUser);
+users.delete('/:id', userController.hardDeleteUser);
+users.delete('/delete/:id', userController.softDeleteUser);
 
 module.exports = users;
