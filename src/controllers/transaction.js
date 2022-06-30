e135bc9eeafb82af1185090945e8636ab41a7b44
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-30 16:40:40 +0700

    fix: update transaction controller

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,8 +1,37 @@
 const response = require('../helpers/standartResponse');
 
+const transactionModel = require('../models/transaction');
+
+
+exports.createTransaction = (req, res) => {
+  transactionModel.createNewTransaction(req.body, (result)=>{
+    return response(res, 'Data created', result);
+  });
+};
+
+exports.getAllTransaction = (req, res) => {
+  transactionModel.getAllTransaction((result)=>{
+    return response(res, 'This is all transaction', result);
+  });
+};
+
 exports.getTransaction = (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: 'this is work'
+  const {id} = req.params;
+  transactionModel.getTransaction(id, (result)=>{
+    return response(res, 'This is data selected', result[0]);
+  });
+};
+
+exports.updateTransaction = (req, res) => {
+  const {id} = req.params;
+  transactionModel.updateTransaction(id, req.body, (result)=>{
+    return response(res, 'Data updated', result[0]);
+  });
+};
+
+exports.hardDeletedTransaction = (req, res) => {
+  const {id} = req.params;
+  transactionModel.deleteTransaction(id, (result)=>{
+    return response(res, 'Data deleted', result[0]);
   });
 };
