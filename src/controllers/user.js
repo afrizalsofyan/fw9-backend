9bc33b9cf9dddc770ab3862fca108b45dc03e77b
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-30 17:44:46 +0700

    fix: update validation username and email

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,6 +3,8 @@ const userModel = require('../models/users');
 
 const { validationResult } = require('express-validator');
 
+const errorResponse = require('../helpers/errorResponse');
+
 exports.getAllUser = (req, res) => {
   userModel.getAllUsers((result)=>{
     return response(res, 'This is user dataaaa.', result,200);
@@ -22,8 +24,19 @@ exports.createUser = (req, res) => {
   if(!validation.isEmpty()){
     return response(res, 'Error input', validation.array(), 400);
   }
-  userModel.createUser(req.body, (result)=>{
-    return response(res, 'Create user successully!!', result[0]);
+  userModel.createUser(req.body, (err, result)=>{
+    if(err) {
+      if(err.code === '23505' && err.detail.includes('username')){
+        const errData = errorResponse('Username Invalid', 'Username');
+        return response(res, 'Error username', errData, 400);
+      } else if(err.code === '23505' && err.detail.includes('email')){
+        const errData = errorResponse('Email invalid', 'Email');
+        return response(res, 'Error email', errData, 400);
+      }
+      return response(res, 'Error', null, 400);
+    } else {
+      return response(res, 'Create user successully!!', result[0]);
+    }
   });
 };
 
