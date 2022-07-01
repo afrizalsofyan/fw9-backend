8e935f50f0c54858a385937a4516988110664667
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-01 16:15:29 +0700

    fix: update error response

--- src/helpers/errorResponse.js
+++ src/helpers/errorResponse.js
@@ -1,4 +1,6 @@
-const errData = (msg, param, location='body') => [
+const response = require('./standartResponse');
+
+const errorHandling = (msg, param, location='body') => [
   {
     msg,
     param,
@@ -6,4 +8,15 @@ const errData = (msg, param, location='body') => [
   }
 ];
 
-module.exports = errData;
+const errorResponse = (err, res) => {
+  if(err.code === '23505' && err.detail.includes('username')){
+    const errData = errorHandling('Username already axists!!', 'Username');
+    return response(res, 'Error username', errData, 400);
+  } else if(err.code === '23505' && err.detail.includes('email')){
+    const errData = errorHandling('Email already axists!!', 'Email');
+    return response(res, 'Error email', errData, 400);
+  }
+  return response(res, 'Error', null, 400);
+};
+
+module.exports = errorResponse;
