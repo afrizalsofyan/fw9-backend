6fc384db1ccee167da2656fbf9a3e2177aa69786
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-01 16:31:53 +0700

    fix: update for hashing password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -26,14 +26,8 @@ exports.createUser = (req, res) => {
   }
   userModel.createUser(req.body, (err, result)=>{
     if(err) {
-      if(err.code === '23505' && err.detail.includes('username')){
-        const errData = errorResponse('Username Invalid', 'Username');
-        return response(res, 'Error username', errData, 400);
-      } else if(err.code === '23505' && err.detail.includes('email')){
-        const errData = errorResponse('Email invalid', 'Email');
-        return response(res, 'Error email', errData, 400);
-      }
-      return response(res, 'Error', null, 400);
+      console.log(err);
+      return errorResponse(err, res);
     } else {
       return response(res, 'Create user successully!!', result[0]);
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,16 @@ const users = require('express').Router();
 const userController = require('../controllers/user');
 
 const {body} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const createValidator = [
-  body('username').isLength({min: 5}).withMessage('username very short, please add 5 character or more!!'),
-  body('email').isEmail().normalizeEmail().withMessage('invalid email format')
+  body('username').isLength({min: 5}).escape().withMessage('username very short, please add 5 character or more!!'),
+  body('email').isEmail().normalizeEmail().withMessage('invalid email format'),
+  body('password').isLength({min: 6}).withMessage('Password min 6 character')
+    .customSanitizer(async (val)=>{
+      const hash = await bcrypt.hash(val, 10);
+      return hash;
+    })
 ];
 
 users.get('/', userController.getAllUser);
