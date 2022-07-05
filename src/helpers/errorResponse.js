37f1f7a7bc6e26336494c101874b59e5390b39ff
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-05 22:19:33 +0700

    fix: update error response if user id not present

--- src/helpers/errorResponse.js
+++ src/helpers/errorResponse.js
@@ -11,15 +11,18 @@ const errorHandling = (msg, param, location='body') => [
 const errorResponse = (err, res) => {
   if(err.code === '23505' && err.detail.includes('username')){
     const errData = errorHandling('Username already axists!!', 'Username');
-    return response(res, 'Error username', errData, 400);
+    return response(res, 'Error username', errData, null, 400);
   } else if(err.code === '23505' && err.detail.includes('email')){
     const errData = errorHandling('Email already axists!!', 'Email');
-    return response(res, 'Error email', errData, 400);
-  } else if (err.code === '23505' && err.detail.includes('user_id')){
+    return response(res, 'Error email', errData, null, 400);
+  } else if (err.code === '23505' && err.detail.includes('already exists')){
     const errData = errorHandling('User id already axists!!', 'User id');
-    return response(res, 'Error user id', errData, 400);
+    return response(res, 'Error user id', errData, null, 400);
+  } else if (err.code === '23503' && err.detail.includes('not present')){
+    const errData = errorHandling('User id not found!!!', 'User id');
+    return response(res, 'Wrong user id', errData, null, 400);
   }
-  return response(res, 'Error', null, 400);
+  return response(res, 'Error', null, null, 400);
 };
 
 module.exports = errorResponse;
