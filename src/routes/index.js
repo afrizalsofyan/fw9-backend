73edd7b30a3a7d18691f8bc521ac7838d1251f59
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-08 17:09:06 +0700

    fix: update crud end point for admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-router.use('/users', require('./user'));
-router.use('/transactions', require('./transaction'));
-router.use('/profiles', require('./profile'));
-router.use('/type/transactions', require('./type_transaction'));
+router.use('/admin/users', require('./user'));
+router.use('/admin/transactions', require('./transaction'));
+router.use('/admin/profiles', require('./profile'));
+router.use('/admin/type/transactions', require('./type_transaction'));
 
 module.exports = router;
