49fd00a7d80b8e19ea5498df84c11834020235d7
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-08 20:48:15 +0700

    fix: update route for crud admin

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
+const routerAdmin = require('express').Router();
 
-router.use('/users', require('./user'));
-router.use('/transactions', require('./transaction'));
-router.use('/profiles', require('./profile'));
-router.use('/type/transactions', require('./type_transaction'));
+routerAdmin.use('/users', require('./user'));
+routerAdmin.use('/transactions', require('./transaction'));
+routerAdmin.use('/userDetail', require('./profile'));
+routerAdmin.use('/type/transactions', require('./type_transaction'));
 
-module.exports = router;
+module.exports = routerAdmin;
