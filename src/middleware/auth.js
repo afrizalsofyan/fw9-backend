b78ddcd097f098006f1d04d4869609335c06a167
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-10 00:51:08 +0700

    fix: update middleware token from jwt

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const response = require('../helpers/standartResponse');
+
+const auth = (req, res, next) => {
+  if(req.headers.authorization){
+    const authToken = req.headers.authorization;
+    const prefix = 'Bearer';
+    if(authToken.startsWith(prefix)){
+      const token = authToken.slice(prefix.length+1, authToken.length);
+      try {
+        const results = jwt.verify(token, process.env.APP_SECRET || 'secretKey');
+        req.authUser = results;
+        next();
+      } catch (error) {
+        return response(res, 'Token Expired', null, null, 401);
+      }
+    }
+  } else {
+    return response(res, 'Unauthorized', null, null, 401);
+  }
+  
+};
+
+module.exports = auth;
