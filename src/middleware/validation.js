bfedf7a5357a2cb4b665c96db5debbf64b767c54
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-10 03:46:59 +0700

    fix: update new validation

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -4,7 +4,7 @@ const response = require('../helpers/standartResponse');
 const profileValidation = (req, res, next) => {
   const error = validationResult(req);
   if(!error.isEmpty()) {
-    return response(res, 'validation error', error.array(), 400);
+    return response(res, 'validation error', error.array(), null, 400);
   }
   next();
 };
