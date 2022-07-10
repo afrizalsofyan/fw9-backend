f5bef17f34c58b7f46bf9061b7a8025b84c1dadf
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-10 16:21:35 +0700

    fix: update route auth

--- src/routes/client/auth.js
+++ src/routes/client/auth.js
@@ -3,12 +3,13 @@ const auth = require('express').Router();
 const authController = require('../../controllers/client/auth');
 const validation = require('../../middleware/validation');
 const validatorRules = require('./validator');
-// const authMiddleware = require('../../middleware/auth');
 
 auth.post('/register', validatorRules.validatorAuth, validation, authController.register);
 auth.post('/createPin', validatorRules.validatorPin, validation, authController.createPin);
 auth.post('/login', validatorRules.validatorLogin, validation, authController.login);
 auth.post('/forgetPasswordLink', validatorRules.validatorEmail, validation, authController.sendEmailForgetPassword);
-auth.post('/forgetPassword', validatorRules.validatorForgetPassword, validation, authController.forgetPassword);
+auth.patch('/forgetPassword', validatorRules.validatorForgetPassword, validation, authController.forgetPassword);
+// auth.post('/confrimEmail', authController.confirmEmail);
 
+// auth.patch('/profile', authMiddleware, authController.upatePofile);
 module.exports = auth;
