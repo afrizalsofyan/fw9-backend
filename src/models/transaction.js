5c9bb9c56d24e3c217ead52ba252c0ee2fee84d1
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-11 01:20:47 +0700

    fix: update model get all transaction

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -12,13 +12,17 @@ exports.createNewTransaction = (data, cb) => {
   });
 };
 
-exports.getAllTransaction = (keyword, searchBy, sortBy, sortType, limit, offset, cb) => {
-  const q = `SELECT * FROM transaction ${!searchBy ? 'ORDER BY id ASC' : `WHERE 
-  ${searchBy == 'amount' ? 'amount::text' : searchBy} 
-  LIKE '%${keyword}%' ORDER BY ${sortBy == 'amount' ? sortBy : 'id'} ${sortType}`} LIMIT $1 OFFSET $2 `;
-  const val = [limit, offset];
-  // const q = 'SELECT id, amount, notes, sender_id, recipient_id, time_transaction::timestamp AT time zone \'GMT+0\' AS time_transaction FROM transaction';
+exports.getAllTransaction = (keyword, searchBy, sortBy, sortType, limit, offset, id, cb) => {
+  if(sortBy=='time'){
+    sortBy='time_transaction';
+  }
+  const q = `SELECT * FROM transaction WHERE sender_id=$1 
+  ${searchBy != null ? 'AND' `(${searchBy} LIKE '%${keyword}%')`: ''} 
+  ORDER BY ${sortBy==null? 'time_transaction DESC' : `${sortBy} ${sortType}`}
+  LIMIT $2 OFFSET $3`;
+  const val = [id, limit, offset];
   db.query(q, val, (err, result)=>{
+    console.log(err);
     cb(err, result.rows);
   });
 };
@@ -77,3 +81,25 @@ exports.softDelete = (id, cb) => {
     }
   });
 };
+
+exports.createTransaction = (time, data, id, cb) => {
+  const q = 'INSERT INTO transaction(time_transaction, notes, amount, type_id, recipient_id, sender_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+  const val = [time, data.notes, data.amount, data.type_id, data.recipient_id, id];
+  db.query(q, val, (err, result)=>{
+    if(err) {
+      cb(err);
+    } else {
+      cb(err, result.rows);
+    }
+  });
+};
+
+// exports.amountCalculate = (senderId, recipientId, amountSender, amountRecipient, cb) => {
+//   db.query('BEGIN', err => {
+//     if (err) {
+//       cb(err);
+//     }
+//     const q = 'UPDATE transaction SET amount=$1, type_id=$4, recipient_id=$5, sender_id=$6 WHERE id=$7 RETURNING *';
+//   const val = [data.time, data.notes, data.amount, data.type_id, data.recipient_id, data.sender_id, id];
+//   })
+// }
