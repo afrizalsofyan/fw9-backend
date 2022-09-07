a8f757ddc35b49f66b2e75cdbfae27871edfe2e4
afrizalsofyan <afrizalafaandy@gmail.com>
2022-09-07 17:53:14 +0700

    add new firebase

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-var serviceAccount = require('../../ourpocket-fb-firebase-adminsdk-w1h08-dc1a219d2e.json');
+var serviceAccount = require('../../ourpocket-55940-firebase-adminsdk-rgzki-bdd0c4be30.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
